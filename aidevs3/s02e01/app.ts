856075a8c9fc716af3d1617d5d4ed845e27d0f9c
Wojciech Piwonski <piwonski.wojciech@gmail.com>
2025-05-19 16:32:27 +0200

    Performed investigation S02E01 completed

--- aidevs3/s02e01/app.ts
+++ aidevs3/s02e01/app.ts
@@ -1,8 +1,17 @@
 import { OpenAIService } from "../../audio/OpenAIService";
 import * as fs from 'fs';
 import * as path from 'path';
-
+import { ExpenseCounter } from "../shared/ExpenseCounter";
+import type OpenAI from "openai";
+import { HeadquartersService } from "../shared/HeadquartersService.ts";
+import { RequestService } from "../shared/RequestService";
+import { cachedTranscriptions } from "./transcriptions";
 const openaiService = new OpenAIService();
+
+const expenseCounter = new ExpenseCounter();
+
+const requestService = new RequestService();
+const headquartersService = new HeadquartersService(requestService);
 
 function getAudioFiles(): string[] {
     // Since we're in aidevs3/s02e01, we'll look in the audio subdirectory
@@ -22,8 +31,9 @@ function getAudioFiles(): string[] {
 
 async function transcriptAudioFile(file: string, buffer: Buffer) {
     try {
+        console.log(`Started transcription for ${file}:`);
         const transcription = await openaiService.transcribeGroq(buffer);
-        console.log(`Transcription for ${file}:`, transcription);
+        console.log(`Transcription for ${file} finished`);
         return {
             file,
             transcription
@@ -36,17 +46,87 @@ async function transcriptAudioFile(file: string, buffer: Buffer) {
         };
     }
 }
-async function main() {
+
+async function investigateAddress(prompt: string): Promise<string | null> {
+    console.log('Investigating...');
+
+    const model = 'gpt-4o';
+    const response = await openaiService.completion({
+        messages: [{
+            role: 'system',
+            content: prompt
+        }],
+        jsonMode: true,
+        model
+    }) as OpenAI.Chat.Completions.ChatCompletion;
+
+    expenseCounter.increaseCost(response);
+
+    return response.choices[0].message.content;
+}
+
+const prompt = `
+    Otrzymujesz listę transkrypcji wywiadów z osobami, które znają Andrzeja Maja.
+    Nie używaj żadnych innych informacji o Andrzeju Maju niż te zawarte w transkrypcjach.
+    Twoim zadaniem jest znalezienie ulicy instytutu uczelni, gdzie wykłada Andrzej Maj.
+    Możesz korzystać z własnej wiedzy do szukania informacji o uczelniach i instytutach w Polsce.
+    Kolejne kroki rozumowania umieść w polu "thinking" JSONa.
+    
+    Odpowiedź przedstaw jako JSON o następującej strukturze:
+    
+    {
+        "thinking": "<kolejne kroki rozumowania>",
+        "answer": "<ulica instytutu uczelni, gdzie wykłada Andrzej Maj>"
+    }
+    
+    W polu "answer" podaj tylko nazwę ulicy, bez dodatkowych informacji.    
+
+    Zwróć tylko JSONa, bez dodatkowych informacji.
+        
+    Transkrypcje:
+    {{transcriptions}}
+`
+
+async function prepareTranscriptions(useCache: boolean = false) {
+    if (useCache) {
+        return cachedTranscriptions;
+    }
+
     // Read files and create buffers
-    const transcriptions = await Promise.all(getAudioFiles().map(async (file: string) => {
+    return await Promise.all(getAudioFiles().map(async (file: string) => {
         const filePath = path.join(__dirname, 'audio', file);
         const buffer = fs.readFileSync(filePath);
         
         return await transcriptAudioFile(file, buffer);
     }));
+}
 
+interface InvestigationAnswer {
+    thinking: string;
+    answer: string;
+}
+
+async function main() {
+    const transcriptions = await prepareTranscriptions();
     console.log('All transcriptions:', transcriptions);
 
+
+    const promptWithTranscriptions = prompt.replace('{{transcriptions}}', transcriptions.map(t => t.transcription).join('\n'));
+
+    const investigationResponse = await investigateAddress(promptWithTranscriptions) ?? '{}';
+    console.log('Investigation response:', investigationResponse);
+    
+
+    const answer: InvestigationAnswer = JSON.parse(investigationResponse);
+    console.log('Answer:', answer);
+
+
+    const street = answer.answer;
+
+    const headquartersResponse = await headquartersService.report('mp3', street);
+    console.log(headquartersResponse);
+    
+    console.log("Used tokens: " + JSON.stringify(expenseCounter.getUsedTokens()));
 }
 
 await main();
