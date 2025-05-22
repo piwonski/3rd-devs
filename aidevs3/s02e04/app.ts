275fb1df266015bc816074c8bcbecb84204508d9
Wojciech Piwonski <piwonski.wojciech@gmail.com>
2025-05-22 11:15:01 +0200

    Send empty response to headquarters

--- aidevs3/s02e04/app.ts
+++ aidevs3/s02e04/app.ts
@@ -1,12 +1,34 @@
 import fs from "fs";
 import path from "path";
+import {HeadquartersService} from "../shared/HeadquartersService.ts";
+import {RequestService} from "../shared/RequestService.ts";
 
 const inputDirectoryName = 'input-files';
+
+interface FactoryAnswer {
+    people: string[];
+    hardware: string[];
+}
+
+const requestService = new RequestService();
+const headquartersService = new HeadquartersService(requestService);
+
+function prepareFactoryAnswer(): FactoryAnswer {
+    return {
+        people: [],
+        hardware: [],
+    }
+}
 
 async function main() {
     const mapFiles = fs.readdirSync(path.join(__dirname, inputDirectoryName))
         .map(file => path.join(__dirname, inputDirectoryName, file));
     console.log('Map files:', mapFiles);
+
+    const answer: FactoryAnswer = prepareFactoryAnswer();
+
+    const headquartersResponse = await headquartersService.report('kategorie', answer);
+    console.log('Headquarters response:', headquartersResponse);
 }
 
 await main();
